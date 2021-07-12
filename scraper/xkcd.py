4a827d80d2bf96be88030310fbe1941d3ce31366
MilaDog <daniel.ryan.sergeant@gmail.com>
2021-07-12 16:41:10 +0200

    Added save_path w/ default, comic folder creation

--- scraper/xkcd.py
+++ scraper/xkcd.py
@@ -9,34 +9,37 @@ class XKCD():  # noqa: D101
         self.process = process
         self.accepted_formats = ['json', 'csv']
 
-    def xkcd_crawl(self, start: Optional[int], finish: Optional[int],  # noqa: D102
+    def __xkcd_crawl(self, start: Optional[int], finish: Optional[int],  # noqa: D102
                    save_path: Optional[str], file_format: Optional[str]) -> None:
         self.process.crawl(XKCDSpider, start, finish, save_path, file_format)
         return
 
-    def xkcd_certain(self,  # noqa: D102
-                     start: Optional[int] = None, file_format: Optional[str] = None
+    def get_certain(self,  # noqa: D102
+                     start: Optional[int] = None, 
+                     file_format: Optional[str] = None,
+                     save_path: Optional[str] = None
                      ) -> None:  # file_format currently taking json and csv. Default is json
         if start is None:
             return
 
         finish = start + 1
         file_format = self.accepted_formats[0] if file_format not in self.accepted_formats else file_format
-        self.xkcd_crawl(start, finish, None, file_format)
+        self.__xkcd_crawl(start, finish, save_path, file_format)
 
-    def xkcd_range(self,  # noqa: D102
+    def get_range(self,  # noqa: D102
                    start: Optional[int] = None, finish: Optional[int] = None,
-                   file_format: Optional[str] = None) -> None:
+                   file_format: Optional[str] = None,
+                   save_path: Optional[str] = None) -> None:
         if start is None or finish is None:
             return
 
         file_format = self.accepted_formats[0] if file_format not in self.accepted_formats else file_format
-        self.xkcd_crawl(start, finish, None, file_format)
+        self.__xkcd_crawl(start, finish+1, None, file_format) # inclusive 
 
 
 if __name__ == '__main__':
     process = CrawlerProcess()
 
     xkcd = XKCD(process)
-    xkcd.xkcd_range(60, 70, 'json')
+    xkcd.get_range(543, 547, 'json', "")
     process.start()
--- scraper/xkcd_spider.py
+++ scraper/xkcd_spider.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import os
 from typing import Generator, Optional
 
 import scrapy
@@ -11,11 +12,13 @@ class XKCDSpider(scrapy.Spider):  # noqa: D101
     def __init__(self,
                  start: Optional[int] = None, finish: Optional[int] = None,
                  save_path: Optional[str] = None, file_format: Optional[str] = None, **kwargs):
+
         super().__init__(**kwargs)
         self.start_url = 'https://xkcd.com/'
         self.start = start
         self.finish = finish
         self.save_path = save_path
+        self.save_path_original = save_path
         self.file_format = file_format
 
     def start_requests(self) -> Generator[scrapy.http.request.Request, None, None]:  # noqa: D102
@@ -24,35 +27,62 @@ class XKCDSpider(scrapy.Spider):  # noqa: D101
 
     def parse(self,  # noqa: D102
               response: Optional[scrapy.http.response.html.HtmlResponse]) -> scrapy.http.request.Request:
+              
         page = response.url.split("/")[-2]
+
         # extract title
         title = response.xpath('//div[@id="ctitle"]/text()').extract()[0]
+
         # extract text
         text = response.xpath('//div[@id="comic"]//img/@title').extract()[0]
+
         # extract comic
         filename = 'xkcd-' + page + '.png'
         image_url = "http://" + response.xpath('//div[@id="comic"]//img/@src').extract()[0][2:]
+
+        # making folder for files
+        try:
+            parent_dir = "../output" if self.save_path_original is None else self.save_path_original
+            directory = "xkcd-" + page
+            self.save_path = os.path.join(parent_dir, directory)
+            os.mkdir(self.save_path)
+        except FileExistsError:
+            pass
 
         # export to file
         if self.file_format == 'json':
             results = {'title': title, 'text': text, 'image source': image_url}
 
-            with open('xkcd-' + page + '.json', 'w') as f:
-                json.dump(results, f)
+            try:
+                with open(self.save_path + '/xkcd-' + page + '.json', 'w') as f:
+                    json.dump(results, f)
+            except FileNotFoundError:
+                raise "Save path does not exist, or cannot find save path"
 
         elif self.file_format == 'csv':
             headers = ['title', 'text', 'image source']
             data = {'title': title, 'text': text, 'image source': image_url}
 
-            with open('xkcd-' + page + '.csv', 'w', encoding='utf-8', newline='') as f:
-                writer = csv.DictWriter(f, fieldnames=headers)
-                writer.writeheader()
-                writer.writerow(data)
+            try:
+                with open('xkcd-' + page + '.csv', 'w', encoding='utf-8', newline='') as f:
+                    writer = csv.DictWriter(f, fieldnames=headers)
+                    writer.writeheader()
+                    writer.writerow(data)
+            except FileNotFoundError:
+                raise "Save path does not exist, or cannot find save path"
 
-        return scrapy.Request(url=image_url, callback=self.parse_img, cb_kwargs=dict(filename=filename))
+        return scrapy.Request(url=image_url, callback=self.parse_img, cb_kwargs=dict(filename=filename, save_path=self.save_path))
 
+    # Creating image for comic
     def parse_img(self,  # noqa: D102
-                  response: Optional[scrapy.http.response.Response], filename: Optional[str]) -> None:
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+                  response: Optional[scrapy.http.response.Response],
+                  filename: Optional[str],
+                  save_path: Optional[str]) -> None:
+
+        try:
+            with open(save_path + "/" + filename, 'wb') as f:
+                f.write(response.body)
+        except FileNotFoundError:
+            raise "Save path does not exist, or cannot find save path"
+        finally:
+            self.log(f'Saved file {filename}')
